95af3c69a826c26705ffe01324a51557360d800d
Your Name <lujain.momani@hotmail.com>
2024-10-09 17:14:21 +0400

    week4

--- lab1.c
+++ lab1.c
@@ -46,7 +46,8 @@ void check_ing();
 
 int main() {
     char command;
-// add a condition to return 
+    while (1) { 
+        
     printf(" choose what you want :\n1) Order a coffee\n2) Admin mode\n3) Exit program\n");
     scanf(" %c", &command); 
 
@@ -59,13 +60,13 @@ int main() {
             break;
         case '3':
             exit_program();
-            break;
+            return 0;
         default:
             printf("Invalid\n");
             break;
+      }
     }
 
-    return 0;
 }
 
 
@@ -73,10 +74,10 @@ int main() {
 void order_coffee() {
     int choice;
     double price = 0.0;
-
+do {
     printf(" coffee types:\n");
     if (beans >= E_bean && water >= E_water) {
-        printf("1) Espresso - %.2f AED\n", eprice);
+            printf("1) Espresso - %.2f AED\n", eprice);
     } else {
         printf("1) Espresso - Unavailable due to insufficient ingredients.\n");
     }
@@ -95,46 +96,42 @@ void order_coffee() {
     printf("Please choose your coffee (0 to cancel): ");
     scanf("%d", &choice);
 
- if (choice == 1) {
-        beans -= E_bean;
-        water -= E_water;
-    } else if (choice == 2) {
-        beans -= C_beans;
-        water -= C_water;
-        milk -= C_milk;
-    } else if (choice == 3) {
-        beans -= M_beans;
-        water -= M_water;
-        milk -= M_milk;
-        syrup -= M_Syrup;
-    }
 
     switch(choice) {
         case 1:
             if (beans >= E_bean && water >= E_water) {
                 price = eprice;
+                    beans -= E_bean;
+                    water -= E_water;
                 printf("You selected Espresso. Price: %.2f AED.\n", price);
             } else {
                 printf("Sorry, Espresso is unavailable.\n");
-                return;
+                continue ;
             }
             break;
         case 2:
             if (beans >= C_beans && water >= C_water && milk >= C_milk) {
                 price = cprice;
+                    beans -= C_beans;
+                    water -= C_water;
+                    milk -= C_milk;
                 printf("You selected Cappuccino. Price: %.2f AED.\n", price);
             } else {
                 printf(" Cappuccino is unavailable.\n");
-                return;
+                continue;
             }
             break;
         case 3:
             if (beans >= M_beans && water >= M_water&& milk >= M_milk && syrup >= M_Syrup) {
                 price = mprice;
+                    beans -= M_beans;
+                    water -= M_water;
+                    milk -= M_milk;
+                    syrup -= M_Syrup;
                 printf("You selected Mocha. Price: %.2f AED.\n", price);
             } else {
                 printf("Sorry, Mocha is unavailable.\n");
-                return;
+                continue;;
             }
             break;
         case 0:
@@ -142,11 +139,11 @@ void order_coffee() {
             return;
         default:
             printf("Invalid selection.\n");
-            return;
+            continue;
     }
   
 
-     double amount_paid = 0.0, coin,remainder;
+         double amount_paid = 0.0, coin,remainder;
 
         printf("Insert coins (valid coins: 1 or 0.5 AED):\n");
 
@@ -163,41 +160,68 @@ void order_coffee() {
         }
 
         printf("Change:%.2f \t\n Enjoy your coffee!\n",remainder);
+    char order_again;
+        printf("\n Do you like to order another coffee? (y/n): ");
+        scanf(" %c", &order_again);
+    if (order_again != 'y' && order_again != 'Y') {
+            printf("Thank you! Have a great day.\n");
+            return; 
+        }
 
-
+} while(true);
 
 }
   
 
 void admin_mode() {
-
     int pass;
     while (true) {
-        printf(" enter the admin pass: ");
+        printf("Enter the admin password: ");
         scanf("%d", &pass);
 
         if (pass == adminpass) {
             int choice;
+            int choice2;
             while (true) {
                 printf("Admin Menu:\n");
-                printf("1) Exit Admin Mode\n");
+                printf("1) Replenish ingredients\n");
+                
+                printf("2) Check current inventory\n");
+                printf("3) Exit Admin Mode\n");
                 scanf("%d", &choice);
                 switch (choice) {
-                         case 1:
-                        printf("Exiting Admin Mode...\n");
-                        return;
+                   case 1:
+                          printf("1) Manual Replenish\n2) Automatic MAXOUT\n");
+                          scanf("%d", &choice2);
+                          switch (choice2) {
+                              case 1:
+                                  manual_replenish(); 
+                                  break;
+                              case 2:
+                                  replenish_ingredients(); 
+                                  break;
+                              default:
+                                  printf("Invalid choice. Please try again.\n");
+                                  break;
+                          }
+                          break;
+                        
+                    case 2:
+                        
+                         check_ing();
+                        return; 
+                        case 3:
+                       printf("Exiting Admin Mode...\n");
+                        break;
                     default:
-                        printf(" try again.\n");
+                        printf("Invalid selection, try again.\n");
                 }
             }
-            break; 
         } else {
-            printf("Wrong pass, try again.\n");
-
+            printf("Wrong password, try again.\n");
         }
     }
 }
-
 void replenish_ingredients() {
     beans = MAX_BEANS;
     water = MAX_WATER;
@@ -220,9 +244,9 @@ void manual_replenish() {
     
     printf("Ingredients have been replenished to the new levels:\n");
     printf("Beans: %d grams\n", beans);
-    printf("Water: %d milliliters\n", water);
-    printf("Milk: %d milliliters\n", milk);
-    printf("Syrup: %d milliliters\n", syrup);
+    printf("Water: %d ml\n", water);
+    printf("Milk: %d ml\n", milk);
+    printf("Syrup: %d mil\n", syrup);
 }
 void check_ing(){
 printf("the current inventory has Beans: %d grams\n Water: %d milliliters\n Milk: %d milliliters\n Syrup: %d milliliters\n ", beans,water,milk,syrup )
